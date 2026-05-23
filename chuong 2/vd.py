a1cf59ef3b88466b82c84767fb3cffcfb7f57398
quanhuy09 <huy9112006@gmail.com>
2026-05-23 14:32:54 +0700

    Delete chuong 2 directory

--- chuong 2/vd.py
+++
@@ -1,9 +0,0 @@
-a = []
-s = 0
-n = int(input("Nhập số phần tử của dãy số:"))
-for i in range(1,n+1):
-    k = int(input("Nhập phần tử thứ "+str(i)+":"))
-    a.append(k)
-for i in a:
-    s = s+i
-print("Tổng số của dãy số là:"+str(s))
