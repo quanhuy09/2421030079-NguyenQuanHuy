efa5e66e7b53e5321614d627d44cfb718926cc91
quanhuy09 <huy9112006@gmail.com>
2026-05-27 12:36:52 +0700

    Add files via upload

---
+++ chuong 3/vd2pvibien.py
@@ -0,0 +1,7 @@
+b = 20
+def msg():
+    a = 10
+    print("Giá trị của a là",a)
+    print("Giá trị của b là",b)
+msg()
+print(b)
---
+++ chuong 3/vdpvibien.py
@@ -0,0 +1,7 @@
+def msg():
+    a = 10
+    print("Giá trị của a là;",a)
+    return
+msg()
+print(a)
+    
